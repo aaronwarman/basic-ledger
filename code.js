59b97709a4cb1d730dd3f6306559feaf5c394417
Aaron Warman <aaronwarman@gmail.com>
2016-07-20 22:19:41 -0400

    created Account and LineItem constructor functions

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
-console.log('hello');
+var Account = function(name, lineItems) {
+  this.name = name;
+  this.lineItems = lineItems || [];
+  return this;
+};
+
+Account.prototype.register = function(lineItem) {
+  var newLineItems = this.lineItems.reduce(function(acc, lineItem) {
+    return acc + lineItem;
+  }, []);
+  return new Account(this.name, newLineItems);
+};
+
+var LineItem = function(amount, label) {
+  this.amount = amount;
+  this.label = label;
+};
+
+var myAccount = new Account('Aaron');
+console.log(myAccount.lineItems.length);
+myAccount.lineItems.push(new LineItem(200.00, 'for food'));
+console.log(myAccount.lineItems.length);
